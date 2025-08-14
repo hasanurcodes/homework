3a87f54ec4063866dd515d5803a595ee0f979287
hasanurcodes <hasanurfamin2022@gmail.com>
2025-08-14 21:55:51 +0600

    js start and examples

---
+++ hw-2025-08-14/js/script.js
@@ -0,0 +1,23 @@
+// ex1
+function showDate() {
+  document.getElementById("sd").innerHTML = Date();
+}
+
+// ex2
+function changeText() {
+  document.getElementById("ct").innerHTML = "Hello SSB";
+}
+
+// ex3
+function showText() {
+  document.getElementById("st").innerHTML = "Hello SSB";
+}
+
+// ex4
+function bulbOn() {
+  document.getElementById("bulb").src = "../resources/images/js-examples/on.gif";
+}
+
+function bulbOff() {
+  document.getElementById("bulb").src = "../resources/images/js-examples/off.gif";
+}
